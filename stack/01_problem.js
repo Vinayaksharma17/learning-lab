b6cced003ecedbdd0c1d91cc6bab387f2bff0542
Vinayaksharma17 <vvsharma999@gmail.com>
2025-05-06 06:01:39 +0530

    Add problem description and comments for valid parentheses check

--- stack/01_problem.js
+++ stack/01_problem.js
@@ -1,3 +1,10 @@
+// Valid Parentheses in an Expression
+
+//Problem description -
+/** Given a string s representing an expression containing various types of brackets: {}, (), and [],
+ *  the task is to determine whether the brackets in the expression are balanced or not. A balanced 
+ *  expression is one where every opening bracket has a corresponding closing bracket in the correct order. */
+
 let str = "[{()}]"
 // let s = "{([])}"
 
