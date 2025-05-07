2ba006bca7b7939b9fe4419a4cdcec6f4cf35a31
Vinayaksharma17 <vvsharma999@gmail.com>
2025-05-07 07:08:58 +0530

    Refactor comments to clarify time and space complexity of the test function

--- time & space complixity/01_problem.js
+++ time & space complixity/01_problem.js
@@ -1,3 +1,4 @@
+// What is the time complexity of the function test ? What is its space complexity ?
 function test(n) {
     for(let i = 0; i < n; i++){
         for(let j = 0; j < n; j++){
@@ -7,3 +8,6 @@ function test(n) {
 }
 
 console.log(test(5))
+
+// The time complexity of test is O(N^2)
+// The space complexity is O(1) as no additional space proportional to n is used
