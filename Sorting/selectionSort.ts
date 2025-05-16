825d4cc5ec0dd9ab4c2fa32d9169a19c86ea1160
Vinayaksharma17 <vvsharma999@gmail.com>
2025-05-16 07:30:14 +0530

    Add implementation of selection sort algorithm with example usage

---
+++ Sorting/selectionSort.ts
@@ -0,0 +1,20 @@
+function selectionSort(arr: number[]) {
+  const n = arr.length
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+    if (minIndex !== i) {
+      ;[arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+  }
+  return arr
+}
+
+const unsortedArr = [64, 34, 25, 12, 22, 11, 90]
+console.log('This is unsorted Array: ', unsortedArr)
+const selectionSortedArr = selectionSort([...unsortedArr]) //Create a copy to avoid modifying the original
+console.log('Sorted Array using Selection Sort: ', selectionSortedArr)
