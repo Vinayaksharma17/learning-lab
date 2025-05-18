db4fa3fad32fe378ea3f0d9891742264161b9106
Vinayaksharma17 <vvsharma999@gmail.com>
2025-05-18 07:05:30 +0530

    Add implementation of quick sort algorithm with example usage

---
+++ Sorting/quickSort.ts
@@ -0,0 +1,23 @@
+function quickSort(arr: number[]) {
+  if (arr.length <= 1) return arr
+
+  const pivot = arr[arr.length - 1]
+  const left: number[] = []
+  const right: number[] = []
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+
+  const sortedLeft = quickSort(left)
+  const sortedRight = quickSort(right)
+
+  return [...sortedLeft, pivot, ...sortedRight]
+}
+
+const unsortedArrayyyy = [64, 34, 25, 12, 22, 11, 90, 10]
+console.log(quickSort(unsortedArrayyyy))
