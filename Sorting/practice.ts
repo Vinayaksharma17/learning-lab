22efa4f8ece9113e14871ac055e57af59a238e1e
Vinayaksharma17 <vvsharma999@gmail.com>
2025-05-21 07:24:52 +0530

    practice sorting function

--- Sorting/practice.ts
+++ Sorting/practice.ts
@@ -37,10 +37,11 @@ const selectionSort1 = (arr) => {
 
 // console.log(selectionSort1(nums))
 
-const insertionSort1 = (arr) => {
+function insertionSort(arr) {
   const l = arr.length
+  let currentElement
   for (let i = 1; i < l; i++) {
-    const currentElement = arr[i]
+    currentElement = arr[i]
     let j = i - 1
     while (j >= 0 && arr[j] > currentElement) {
       arr[j + 1] = arr[j]
@@ -51,4 +52,44 @@ const insertionSort1 = (arr) => {
   return arr
 }
 
-console.log(insertionSort1(nums))
+function mergeSort(arr: number[]): number[] {
+  if (arr.length <= 1) return arr
+
+  const mid = Math.floor(arr.length / 2)
+  const left = arr.slice(0, mid)
+  const right = arr.slice(mid)
+
+  const sortedLeft = mergeSort(left)
+  const sortedRight = mergeSort(right)
+
+  return merge(sortedLeft, sortedRight)
+}
+
+function merge(arr1: number[], arr2: number[]): number[] {
+  const result: number[] = []
+  let i = 0
+  let j = 0
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i])
+      i++
+    } else {
+      result.push(arr2[j])
+      j++
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i])
+    i++
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j])
+    j++
+  }
+  return result
+}
+
+console.log(mergeSort([29, 10, 14, 37, 14]))
