254f2e00c8118b4fd1b01834f7d0ab15cc698687
Vinayaksharma17 <vvsharma999@gmail.com>
2025-07-01 20:32:24 +0530

    Add implementation to find the second largest distinct element in an array

---
+++ Arrays and Strings/Array Problems/prob_1.ts
@@ -0,0 +1,26 @@
+/**
+ * Given an array of positive integers arr[] of size n, the task is to find 
+ * second largest distinct element in the array.
+
+Note: If the second largest element does not exist, return -1.
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+ */
+
+function findSecondLargestEle(arr: any) {
+    let element: any = 0
+  let largestEle: any
+
+  for (let i = 0; i < arr.length; i++) {
+
+  }
+}
+
+const arr = [12, 35, 1, 10, 34, 1]
