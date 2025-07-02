9cf789097599ab832600d5f77dfb57a111701372
Vinayaksharma17 <vvsharma999@gmail.com>
2025-07-02 07:11:29 +0530

    Refactor findSecondLargestEle function to improve clarity and maintainability

--- Arrays and Strings/Array Problems/prob_1.ts
+++ Arrays and Strings/Array Problems/prob_1.ts
@@ -14,13 +14,25 @@ Explanation: The largest element of the array is 10 and the second largest eleme
 
  */
 
+// Approach 1 - Naive Approach - Using sorting
 function findSecondLargestEle(arr: any) {
-    let element: any = 0
-  let largestEle: any
+  const n = arr.length
 
-  for (let i = 0; i < arr.length; i++) {
+  // Sort the array in non-decreasing order - 10,10, 20, 30, 40, 50, 70,
+  arr.sort((a: number, b: number) => a - b)
 
+  // start from second largest element as last element is the largest
+  for (let i = n - 2; i >= 0; i--) {
+    // return the first element which is not equal to the largest element
+    if (arr[i] !== arr[n - 1]) {
+      return arr[i]
+    }
   }
+  // If no second largest element was found, return -1
+  return -1
 }
 
 const arr = [12, 35, 1, 10, 34, 1]
+console.log(findSecondLargestEle(arr))
+
+// Time Complexity: O(n*log(n))
