35b2eb6fc777363272333d7a44f1b20eec4ad710
Vinayaksharma17 <vvsharma999@gmail.com>
2025-07-03 21:27:23 +0530

    Add implementation to find the second largest element using two traversals

--- Arrays and Strings/Array Problems/prob_1.ts
+++ Arrays and Strings/Array Problems/prob_1.ts
@@ -36,3 +36,33 @@ const arr = [12, 35, 1, 10, 34, 1]
 console.log(findSecondLargestEle(arr))
 
 // Time Complexity: O(n*log(n))
+
+
+//Approach 2: 
+// JavaScript program to find the second largest element in the array
+// using two traversals
+
+function getSecondLargest(arr) {
+  let n = arr.length
+
+  let largest = -1,
+    secondLargest = -1
+
+  // Finding the largest element
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > largest) largest = arr[i]
+  }
+
+  // Finding the second largest element
+  for (let i = 0; i < n; i++) {
+    // Update second largest if the current element is greater
+    // than second largest and not equal to the largest
+    if (arr[i] > secondLargest && arr[i] !== largest) {
+      secondLargest = arr[i]
+    }
+  }
+  return secondLargest
+}
+
+let arr1 = [12, 35, 1, 10, 34, 1]
+console.log(getSecondLargest(arr1))
