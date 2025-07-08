02fb15c4a12e9b5a36225b233c9707691c61bfc0
Vinayaksharma17 <vvsharma999@gmail.com>
2025-07-08 07:35:24 +0530

    Add implementation to find the smallest and second smallest elements in an array

--- Arrays and Strings/Array Problems/prob_2.ts
+++ Arrays and Strings/Array Problems/prob_2.ts
@@ -15,3 +15,39 @@ Output: [2, 3]
 Explanation: 2 and 3 are respectively the smallest and second smallest elements in the array.
  */
 
+function findSmallestEle(arr: any) {
+  const sortedArr = arr.sort((a, b) => a - b)
+  return [sortedArr[0], sortedArr[1]]
+}
+
+// const arr2 = [12, 25, 8, 55, 10, 33, 17, 11]
+// console.log(findSmallestEle(arr2))
+
+//Approach 2:
+function minAnd2ndMin(arr) {
+  let n = arr.length
+  // Need at least two elements to have a second minimum
+  if (n < 2) return -1
+
+  // Initialize first and second minimums to "infinity"
+  let first = Number.MAX_SAFE_INTEGER
+  let second = Number.MAX_SAFE_INTEGER
+
+  for (let i = 0; i < n; i++) {
+    if (arr[i] < first) {
+      second = first
+      first = arr[i]
+    } else if (arr[i] < second && arr[i] != first) {
+      second = arr[i]
+    }
+  }
+
+  if (second === Number.MAX_SAFE_INTEGER) {
+    return -1
+  }
+
+  return [first, second]
+}
+
+const arr2 = [12, 25, 8, 55, 10, 33, 17, 11]
+console.log(minAnd2ndMin(arr2))
